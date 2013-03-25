265dd6522de571a71d149108e84e0f56e09d7274
zed <aaron.zeisler@gmail.com>
2013-03-24 23:27:48 -0700

    Wrote the code to set up a new game.

--- dominion/card.py
+++ dominion/card.py
@@ -2,10 +2,14 @@
 class Card(object):
 
     Name = None
+    Type = None
     Cost = None
-    VictoryPoints = None
+    Effects = None
+    ImageURL = None
 
-    def __init__(self, name=None, cost=None, victoryPoints=None):
+    def __init__(self, name=None, type=None, cost=None, effects=None, imageUrl=None):
         self.Name = name
+        self.Type = type
         self.Cost = cost
-        self.VictoryPoints = victoryPoints
+        self.Effects = effects
+        self.ImageURL = imageUrl
